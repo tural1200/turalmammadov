d9ebaddebe97bf7d43f22d05d3c51a45d534e21e
tural1200 <tural1200@gmail.com>
2025-08-11 11:31:58 +0400

    feat: add social media links data for GitHub, Instagram, and LinkedIn in Hero component

---
+++ src/modules/home/components/hero/data.ts
@@ -0,0 +1,21 @@
+import GithubIcon from "@/components/icons/GithubIcon";
+import InstagramIcon from "@/components/icons/InstagramIcon";
+import LinkedinIcon from "@/components/icons/LinkedinIcon";
+
+export const socialLinks = [
+  {
+    label: "GitHub",
+    href: "https://github.com/tural1200",
+    icon: GithubIcon,
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/tural-mammadov-69b558143/",
+    icon: LinkedinIcon,
+  },
+  {
+    label: "Instagram",
+    href: "https://www.instagram.com/i.am.tural/",
+    icon: InstagramIcon,
+  },
+];
