140a51184e4c6c6db58291a69c6c048259cfe23b
tural1200 <tural1200@gmail.com>
2025-08-11 11:32:02 +0400

    feat: enhance Hero component with social media links and layout adjustments

--- src/modules/home/components/hero/Hero.tsx
+++ src/modules/home/components/hero/Hero.tsx
@@ -1,6 +1,8 @@
+import Profile from "@/modules/home/components/profile";
 import ButtonLink from "@/components/button-link";
 import DownloadIcon from "@/components/icons/DownloadIcon";
-import Profile from "@/components/profile";
+import { socialLinks } from "./data";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -12,22 +14,37 @@ const Hero = () => {
       <p className="text-2xl font-normal mb-[1.875rem] animate-fade-in-up delay-200">
         Hi! I&apos;m Tural 👋🏻
       </p>
-      <p className="text-[2rem] font-semibold max-w-[26.75rem] text-center mb-[1.875rem] animate-fade-in-up delay-400">
-        I&apos;m a Frontend Developer based in Baku, Azerbaijan
+      <p className="text-[2rem] font-semibold max-w-[26.75rem] mb-[1.875rem] animate-fade-in-up delay-400">
+        Frontend Developer based in Baku, Azerbaijan
       </p>
-      <p className="text-[1rem] leading-[1.5] font-normal max-w-[26.75rem] text-center mb-[2.5rem] animate-fade-in-up delay-600">
+      <p className="text-[1rem] leading-[1.5] font-normal max-w-[26.75rem] mb-[1.5rem] animate-fade-in-up delay-600">
         Experienced front-end developer with a passion for crafting elegant and
         efficient user interfaces. Over three years of hands-on experience in
         web development.
       </p>
-      <div className="flex flex-wrap justify-center gap-[1.25rem] animate-fade-in-up delay-800">
-        <ButtonLink href="mailto:info@turalmammadov.dev">Contact Me</ButtonLink>
+      <div className="flex self-start gap-[0.75rem] mb-[1.5rem] animate-fade-in-up delay-600">
+        {socialLinks.map((link) => (
+          <Link
+            href={link.href}
+            key={link.label}
+            target="_blank"
+            className="w-6 h-6 flex items-center justify-center"
+          >
+            <link.icon className="w-6 h-6" />
+          </Link>
+        ))}
+      </div>
+      <div className="w-full flex justify-center gap-[1.25rem] [@media(max-width:48rem)]:flex-wrap animate-fade-in-up delay-800">
+        <ButtonLink href="mailto:info@turalmammadov.dev" className="w-full">
+          Contact Me
+        </ButtonLink>
         <ButtonLink
           href="/pdf/Tural-Mammadov-CV.pdf"
           variant="secondary"
           download
+          className="w-full"
         >
-          <span className="font-normal text-base flex items-center gap-[0.5rem] ">
+          <span className="font-normal text-base flex justify-center items-center gap-[0.5rem] ">
             Download CV
             <DownloadIcon />
           </span>
